8d9bc0ff29ddbffb6be1e4bc1b58f501ac7e770b
Dragos-Stefan Alexa <drgs_lx@yahoo.com>
2024-07-05 09:59:11 +0300

    modified the color

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -17,7 +17,7 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   return createPortal(
     <dialog
       ref={dialog}
-      className="backdrop:bg-stone-800/80 p-6 rounded-md shadow-md"
+      className="backdrop:bg-stone-900/90 p-6 rounded-md shadow-md"
     >
       {children}
       <form method="dialog" className="mt-2 text-right">
